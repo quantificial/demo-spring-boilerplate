d5905a61cfb08efd3dbb7cdb462484e7c5c30a65
Johnson Fu <johnson.fu@gmail.com>
2019-07-24 18:15:34 +0800

    add list headers function

--- src/main/java/demo/demospringboilerplate/DefaultWebController.java
+++ src/main/java/demo/demospringboilerplate/DefaultWebController.java
@@ -1,14 +1,21 @@
 package demo.demospringboilerplate;
 
 import java.util.Date;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class DefaultWebController {
 	
     private String appMode;
@@ -16,6 +23,17 @@ public class DefaultWebController {
     @Autowired
     public DefaultWebController(Environment environment){
         appMode = environment.getProperty("app-mode");
+    }
+    
+    @RequestMapping("/listHeaders")
+    public ResponseEntity<String> listAllHeaders(@RequestHeader Map<String, String> headers) {
+        headers.forEach((key, value) -> {
+        	
+            log.info(String.format("Header '%s' = %s", key, value));
+           
+        });
+     
+        return new ResponseEntity<String>(String.format("Listed %d headers", headers.size()), HttpStatus.OK);
     }
 
     @RequestMapping("/")
